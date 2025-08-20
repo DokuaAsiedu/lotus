da7c456cd7c394f17d820141b1f1ac7c2cc7c046
Dokua <akua.dokua@imedicaf.com>
2025-08-20 16:10:17 +0000

    style: layout changes

--- src/components/sales.tsx
+++ src/components/sales.tsx
@@ -61,23 +61,23 @@ export function Tickets() {
       {Array.from({ length: 10 }, (_, index) => {
         return (
           <div key={`index-${index}`} className="p-6 grid grid-cols-9 gap-4 items-center text-sm border-t-1 border-t-light-gray">
-            <div className="col-span-2 flex flex-col">
+            <div className="col-span-2 flex flex-col gap-2">
               <span>1024-1034589</span>
-              <span className={`text-smokey-gray text-xs ${montserrat.className}`}>Mac 5 Original</span>
+              <span className={`text-smokey-gray text-2xs ${montserrat.className}`}>Mac 5 Original</span>
             </div>
             <div className="col-span-1">Direct 5</div>
-            <div className="col-span-3 flex gap-2">
-              {[44, 33, 89, 21, 7].map((item, index) => {
+            <div className="col-span-3 flex items-center flex-wrap gap-2">
+              {[44, 33, 89, 21, 17].map((item, index) => {
                 return (
-                  <div key={`index-${index}`} className="grow grid place-items-center border-1 border-light-gray rounded-md">{item}</div>
+                  <div key={`index-${index}`} className="grow aspect-square grid place-items-center border-1 border-light-gray rounded-md font-semibold">{item}</div>
                 )
               })}
             </div>
             <div className="col-span-3 flex items-center gap-3">
               <div className="grid place-items-center">
-                <Image src="/avatar-sheena.png" alt="qr code" height={30} width={30} />
+                <Image src="/avatar-sheena.png" alt="qr code" height={30} width={30} className="h-full aspect-square"/>
               </div>
-              <div className="flex flex-col justify-center gap-1">
+              <div className={`${montserrat.className} flex flex-col justify-center gap-1`}>
                 <span>Sheena Osei</span>
                 <span className="text-smokey-gray">+233 (0)24 567 8901</span>
               </div>
