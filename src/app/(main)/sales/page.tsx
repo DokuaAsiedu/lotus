8782d8ce79996523943d006c7cb1e55c4165d062
Dokua <akua.dokua@imedicaf.com>
2025-08-20 09:52:27 +0000

    feat: add winnings component

--- src/app/(main)/sales/page.tsx
+++ src/app/(main)/sales/page.tsx
@@ -1,9 +1,9 @@
-import { Retailers, Summary, Tickets } from '@/components'
+import { Retailers, Summary, Tickets, Winnings } from '@/components'
 
 export default function Sales() {
   return (
-    <div className="p-4 w-full grid md:grid-cols-11 gap-4">
-      <div className="col-span-11 lg:col-span-5 flex flex-col items-stretch flex-wrap gap-6">
+    <div className="p-4 w-full grid xl:grid-cols-12 gap-4">
+      <div className="col-span-12 xl:col-span-5 flex flex-col items-stretch flex-wrap gap-6">
         <div className="px-4 py-2 border-light-gray border-1 rounded-md">
           <Summary />
         </div>
@@ -11,10 +11,12 @@ export default function Sales() {
           <Tickets />
         </div>
       </div>
-      <div className="col-span-11 lg:col-span-4 border-light-gray border-1 rounded-md">
+      <div className="col-span-12 xl:col-span-4 border-light-gray border-1 rounded-md">
         <Retailers />
       </div>
-      <div className="col-span-11 lg:col-span-2 border-light-gray border-1 rounded-md"></div>
+      <div className="col-span-12 xl:col-span-3 border-light-gray border-1 rounded-md">
+        <Winnings />
+      </div>
     </div>
   )
 }
--- src/components/sales.tsx
+++ src/components/sales.tsx
@@ -8,7 +8,7 @@ const jura = Jura({
 })
 
 const inter = Inter({
-  weight: ['400', '700'],
+  weight: ['400', '600', '700'],
   subsets: ['latin'],
 })
 
@@ -132,6 +132,68 @@ export function Retailers() {
               </div>
               <div className={`${jura.className} col-span-2`}>GHS 3,012.00</div>
               <div className={`${inter.className} col-span-1`}>14</div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export function Winnings() {
+  return (
+    <div>
+      <div className="px-6 py-4 flex flex-col gap-2 border-b-1 border-b-light-gray">
+        <div className="grid grid-cols-2">
+          <div className={`${golosText.className} flex flex-col justify-between gap-4`}>
+            <h2 className="font-semibold text-xl">Winnings</h2>
+            <p className="font-bold text-smokey-gray text-xs">Game</p>
+          </div>
+          <div className={`${montserrat.className} text-smokey-gray`}>
+            <span className="text-xs font-bold">Draw Date</span>
+            <div className="p-2 flex gap-2 border-light-gray border-1 rounded-sm text-black">
+              <span>02/04/2025 |</span>
+              <div className="h-full aspect-square grid place-items-center">
+                <Image src="/icons/calendar.png" alt="calendaricon" height={5} width={5} layout="responsive" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={`${montserrat.className} flex items-center justify-between`}>
+          <span className="font-normal">5/90 Original +</span>
+          <p className="text-xs font-bold text-end text-smokey-gray">
+            Event #: <span className="text-black">91023</span>
+          </p>
+        </div>
+      </div>
+      <div className={`${montserrat.className} px-6 py-4 flex items-center gap-4 border-b-1 border-b-light-gray`}>
+        <span className="font-bold">5/90 Original:</span>
+        <div className="grow flex gap-2">
+          {[44, 33, 89, 21, 7].map((item, index) => {
+            return (
+              <div key={`index-${index}`} className={`${inter.className} grow w-full grid place-items-center aspect-square bg-black text-white rounded-sm font-semibold`}>{item}</div>
+            )
+          })}
+        </div>
+      </div>
+      <div className="px-6">
+        {Array.from({ length: 10 }, (_, index) => {
+          return (
+            <div key={`index-${index}`} className="py-4 grid grid-cols-2 gap-2 border-b-1 border-b-[#E0E0E0]">
+              <div className="flex flex-col gap-2">
+                <div className="col-span-1 flex gap-2">
+                  {[44, 33, 89, 21, 7].map((item, index) => {
+                    return (
+                      <div key={`elem-${index}`} className={`${inter.className} grid place-items-center aspect-square rounded-sm border-1 border-light-gray font-semibold`}>{item}</div>
+                    )
+                  })}
+                </div>
+                <p className={`${montserrat.className} text-xs text-smokey-gray`}>Perm 3 | 5/90 Original</p>
+              </div>
+              <div className="col-span-1 flex flex-col items-end justify-between">
+                <span className={`${jura.className} text-end`}>GHS 3,012.00</span>
+                <span className={`${montserrat.className} text-smokey-gray text-xs`}>+233 (0)24 567 8901</span>
+              </div>
             </div>
           )
         })}
