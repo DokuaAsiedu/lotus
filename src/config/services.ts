d3dfb4a44403663d591dc5f1ddc1686f8db38fb7
Dokua <akua.dokua@imedicaf.com>
2025-08-25 20:33:02 +0000

    feat: add session functionality

---
+++ src/config/services.ts
@@ -0,0 +1,1 @@
+export const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://18.119.94.68/api"
---
+++ src/lib/session.ts
@@ -0,0 +1,57 @@
+"use server"
+
+import { ResponseCookie } from "next/dist/compiled/@edge-runtime/cookies"
+import { cookies } from "next/headers"
+import { SESSION_EXPIRY_HOURS, SESSION_KEY } from "../config"
+
+type data = {
+  userId: number
+  entityId: never
+  token: string
+  expiresAt: string
+}
+
+const cookieProps: Partial<ResponseCookie> = {
+  httpOnly: true,
+  sameSite: "lax",
+  secure: process.env.NODE_ENV === "production",
+  expires: getExpiryDate(),
+  path: "/",
+}
+
+function getExpiryDate() {
+  return new Date(Date.now() + SESSION_EXPIRY_HOURS * 60 * 60 * 1000)
+}
+
+export async function createSession(data: data) {
+  await storeSessionCookie(data)
+}
+
+async function storeSessionCookie(sessionData: data) {
+  const stringifiedData = JSON.stringify(sessionData)
+  const cookieStore = await cookies()
+
+  cookieStore.set(SESSION_KEY, stringifiedData, cookieProps)
+}
+
+export async function getCookie(key: string) {
+  const cookieStore = await cookies()
+
+  return cookieStore.get(key)?.value
+}
+
+export async function refreshSession() {
+  const session = await getCookie(SESSION_KEY)
+
+  if (!session) return null
+
+  const data = JSON.parse(session);
+
+  await storeSessionCookie(data)
+}
+
+export async function destroySession() {
+  const cookieStore = await cookies()
+
+  cookieStore.delete(SESSION_KEY)
+}
