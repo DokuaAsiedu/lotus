811025daaffd2c8dce021911cd49cabb048372ae
Dokua <akua.dokua@imedicaf.com>
2025-08-28 14:47:27 +0000

    fix: type checking

--- src/providers/state-provider.tsx
+++ src/providers/state-provider.tsx
@@ -5,6 +5,7 @@ import { createContext, useContext, useState } from "react";
 
 const state: State = {
   selectedGames: [],
+  handleSelectedGames: () => {},
 }
 
 const AppStateContext = createContext(state)
